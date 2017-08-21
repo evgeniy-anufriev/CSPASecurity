f81f90d2ac08eb00074e57fff9c3cbf04fe215b5
evgeniy-anufriev <evgeniy.anufriev@gmail.com>
2017-08-21 17:48:08 +0300

    process

--- WindowsFormsApplication5/Form1.Designer.cs
+++ WindowsFormsApplication5/Form1.Designer.cs
@@ -28,30 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.delete = new System.Windows.Forms.ToolStripMenuItem();
+            this.edit = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(692, 487);
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(715, 510);
+            this.dataGridView1.StandardTab = true;
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellMouseDown += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.delete,
+            this.edit});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 70);
+            // 
+            // delete
+            // 
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(152, 22);
+            this.delete.Tag = "delete";
+            this.delete.Text = "delete";
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // edit
+            // 
+            this.edit.Name = "edit";
+            this.edit.Size = new System.Drawing.Size(152, 22);
+            this.edit.Text = "edit";
+            this.edit.Click += new System.EventHandler(this.edit_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(1041, 512);
+            this.splitContainer1.SplitterDistance = 320;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Tag = "protections";
+            this.label2.Text = "groups";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Tag = "users";
+            this.label1.Text = "users";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(692, 487);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(1041, 512);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -59,6 +149,12 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem delete;
+        private System.Windows.Forms.ToolStripMenuItem edit;
     }
 }
 
--- WindowsFormsApplication5/Form1.cs
+++ WindowsFormsApplication5/Form1.cs
@@ -13,24 +13,90 @@ namespace WindowsFormsApplication5
 {
     public partial class Form1 : Form
     {
+        public string ConnStr = "DSN=MySQL";
         public Form1()
         {
             InitializeComponent();
-            FillData();
         }
 
-        void FillData()
+        void FillData(String table)
         {
-            using (OdbcConnection c = new OdbcConnection("DSN=MySQL"))
+            using (OdbcConnection c = new OdbcConnection(ConnStr))
             {
                 c.Open();
-                using (OdbcDataAdapter adapter = new OdbcDataAdapter("Select * from protections", c))
+                using (OdbcDataAdapter adapter = new OdbcDataAdapter("Select * from " + table, c))
                 {
                     DataSet ds = new DataSet();
                     adapter.Fill(ds);
                     dataGridView1.DataSource = ds.Tables[0];
+                    dataGridView1.Tag = table;
+                }
+                c.Close();
+            }
+        }
+
+        void Delete(int row, String table)
+        {
+            String sql = "Delete from " + table + " WHERE id= " + row;
+            CommandSQL(sql);
+        }
+
+        void Add(String row, String table)
+        {
+            String sql = "INSERT INTO " + table + " WHERE id= " + row;
+            CommandSQL(sql);
+        }
+
+        void CommandSQL(String sql)
+        {
+            using (OdbcConnection c = new OdbcConnection(ConnStr))
+            {
+                c.Open();
+                using (OdbcCommand command = new OdbcCommand(sql, c))
+                {
+                    command.ExecuteNonQuery();
+                }
+                c.Close();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            FillData(((Label)sender).Tag.ToString());
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            FillData(((Label)sender).Tag.ToString());
+        }
+
+        private void dataGridView1_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                int rowSelected = e.RowIndex;
+                if (e.RowIndex != -1)
+                {
+                    this.dataGridView1.Rows[rowSelected].Selected = true;
                 }
             }
         }
+
+        private void edit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+    
+            DataGridView ContextMenuOwner = (DataGridView)((ContextMenuStrip)((ToolStripItem)sender).Owner).SourceControl;         
+            Delete(ContextMenuOwner.CurrentCell.RowIndex+1,ContextMenuOwner.Tag.ToString());
+            FillData(ContextMenuOwner.Tag.ToString());
+        }
+
+
+
+
     }
 }
